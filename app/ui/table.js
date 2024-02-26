3f4350060933d6d9060ccafa5a18009b3f0da708
mgiang2015 <mgiang2015@gmail.com>
2024-02-26 12:11:46 +0800

    Add layout to home, news and table

--- app/ui/table.js
+++ app/ui/table.js
@@ -3,21 +3,21 @@ export default function Table({ columns, items }) {
         <table style={{ border: "2px solid" }}>
             <thead>
                 <tr>
-                    {columns.map(column => <th style={{ border: '1px solid' }} key={column.key} scope='col'>{column.label}</th>)}
+                    {columns.map(column => <th style={{ border: '1px solid', padding: "0.5em" }} key={column.key} scope='col'>{column.label}</th>)}
                 </tr>
             </thead>
             <tbody>
                 {items.map((item) => (
-                    <tr key={item.Title}>
-                        <td style={{ border: '1px solid' }}>
+                    <tr key={item.Url}>
+                        <td style={{ border: '1px solid', padding: "0.5em" }}>
                             <a href={item.Url} target='_blank'>
                                 {item.Title}
                             </a>
                         </td>
-                        <td style={{ border: '1px solid' }}>{item.Source}</td>
-                        <td style={{ border: '1px solid' }}>{item.PublishedOn}</td>
-                        <td style={{ border: '1px solid' }}>{item.Country}</td>
-                        <td style={{ border: '1px solid' }}>{item.Description}</td>
+                        <td style={{ border: '1px solid', padding: "0.5em" }}>{item.Source}</td>
+                        <td style={{ border: '1px solid', padding: "0.5em" }}>{item.PublishedOn}</td>
+                        <td style={{ border: '1px solid', padding: "0.5em" }}>{item.Country}</td>
+                        <td style={{ border: '1px solid', padding: "0.5em" }}>{item.Description}</td>
                     </tr>
                 ))}
             </tbody>
